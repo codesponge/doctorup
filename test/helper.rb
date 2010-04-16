5d0585f8ad65f53be6ceaa775864d5468edaaffb
codesponge <billy@codesponge.com>
2010-04-16 12:22:32 -0400

    add some test helpers and snippet tests

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+begin
+  require 'turn'
+rescue LoadError
+  ""
+end
+
+
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
@@ -9,4 +16,23 @@ require 'snippet'
 
 class Test::Unit::TestCase
   DATA_DIR = File.join(File.dirname(__FILE__), 'data')
+  
+  
+  def assert_hashish(object)
+    [:key,:keys,:has_key?].each do |method|
+      assert_respond_to(object, :method)
+    end
+    
+    
+    
+  end
+  
+  def relativly_random_string(chars = 8)
+    ar = [] + ('a'..'z').to_a + ('A'..'Z').to_a
+    os = ""
+    chars.times{ os << ar[rand(ar.size)] }
+    os
+  end
+  
+  
 end
--- test/test_snippet.rb
+++ test/test_snippet.rb
@@ -13,9 +13,7 @@ require 'helper'
 class	 Test_snippet_case < Test::Unit::TestCase
 
 
-  # should "fail" do
-  #   assert false
-  # end
+
   
 	context "An empty Snippet" do
 		setup do
@@ -26,8 +24,6 @@ class	 Test_snippet_case < Test::Unit::TestCase
 			assert_equal(@snippet, @snippet.to_s)
 		end
 	end
-
-
 	  
   context "A snippet with some text" do
     setup do
@@ -40,12 +36,21 @@ class	 Test_snippet_case < Test::Unit::TestCase
     
     
     should "at least claim to respond to some methods" do
-      [:syntax_up,:to_html,:to_s,:sytaxify].each do | method |
+      [:syntax_up,:to_html,:to_s,:sytaxify,:options,:settings].each do | method |
         assert_respond_to(@snippet, method)
       end
     end
     
+    should "have options that behave like a hash" do
+      assert_hashish @snippet
+    end
+    
+    should "have options that are settable" do
+      opt_key,opt_value = relativly_random_string,relativly_random_string
+      @snippet.options[opt_key] = opt_value
+      assert @snippet.options.has_key? opt_key
+      assert_equal(@snippet.options[opt_key], opt_value)
+    end
+    
   end
-  
-	
 end
