7285e8e9b803ffabbe58dc0846f071dc4288a5fe
codesponge <billy@codesponge.com>
2010-04-16 14:16:12 -0400

    changed layout of class file & white space fixes

--- lib/snippet.rb
+++ lib/snippet.rb
@@ -4,31 +4,36 @@
 #   Created by William Champlin on 2010-04-16.
 #   Copyright (c) 2010 CodeSponge. All rights reserved.
 #	
-#-------------------------------------------------------------------------
-<<-DESCRIPTION
+#--------------------------------------------------------------
+
+class Snippet < String 
+
+#--------------------------------------------------------------
+Description=<<-TEXTILE
+
+A snippet is a string that can spit out syntax hightlighted,
+and textilized versions of it's self in HTML. 
+
+The strings value is preserved (syntax is seperate from *Self*).
+
+TEXTILE
+
+#--------------------------------------------------------------
 
 
 
-
-
-DESCRIPTION
-#--------------------------------------------------------------------------
-#
-#-	
-#-------
-
-class Snippet < String
 
   expected_methods = [:syntax_up,:to_html,:to_s,:sytaxify]
   attr_accessor :opts
   alias_method :options, :opts
   alias_method :settings, :opts
   
-  
   def initialize(*args)
+		#FIXME setting opts here is for stub testing only!
     @opts = {:parser => :coderay }
     super
   end
+
   #create a marked up version with syntax highlighting
   #and textile parsed returns true on success and nil on
   #failure
@@ -45,5 +50,6 @@ class Snippet < String
   
   alias_method :sytaxify, :to_html
   
-
-end
+#--------------------------------------------------------------
+end # => class Snippet < String
+#--------------------------------------------------------------
